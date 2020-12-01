8a73f9638bcf4e5e7ab3e90954d6ea61f807d8f6
PameTLujan <53342991+PameTLujan@users.noreply.github.com>
2020-12-01 11:33:17 -0300

    first commit

---
+++ src/main/java/com/sae/biodat/BiodatApplication.java
@@ -0,0 +1,13 @@
+package com.sae.biodat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiodatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiodatApplication.class, args);
+	}
+
+}
